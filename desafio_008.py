4f0e3653ca952c255ae21b28e0e62c3f6859e814
Marina Falcão <marigabrielbrito@gmail.com>
2024-10-25 14:30:09 -0300

    incluí mais desafios

---
+++ desafio_008.py
@@ -0,0 +1,4 @@
+metro = int(input("Digite o comprimento em metros: "))
+cm = metro * 100
+mm = metro * 1000
+print("O valor digitado foi {} metros. Isso corresponde a {} cm e {} mm.".format(metro, cm, mm))
