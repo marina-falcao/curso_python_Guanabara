8c235ade16dee07defe65724822335849741a3d4
marina-falcao <marigabrielbrito@gmail.com>
2024-12-04 17:55:49 -0300

    suggestions in activities

--- desafio_022.py
+++ desafio_022.py
@@ -10,6 +10,10 @@ total = len(nome)
 nome_s_espaco = total - espaco
 print("Seu nome tem {} letras.".format(nome_s_espaco))
 
+#outra forma de fazer: print("Seu nome tem {} letras.".format(len(nome) - nome.count(' ')))
+
 #numero de letras do primeiro nome
 prim_nome = nome.split()
 print("Seu primeiro nome tem" , len(prim_nome[0]), "letras")
+
+#outra forma de fazer: print("Seu primeiro nome tem {} letras.". format(nome.find(' ')))
--- desafio_023.py
+++ desafio_023.py
@@ -4,3 +4,15 @@ print("unidade:", num[-1])
 print("dezena:", num[-2])
 print("centena:", num[1])
 print("milhar:", num[0])
+
+#outra forma de fazer:
+#num = int(input("Digite um numero de 0 a 9999: "))
+#unidade = num // 1 % 10
+#dezena = num // 10 % 10
+#centena = num // 100 % 10
+#milhar = num // 1000 % 10
+
+#print("unidade: {}".format(unidade))
+#print("dezena: {}".format(dezena))
+#print("centena: {}".format(centena))
+#print("milhar: {}".format(milhar))
